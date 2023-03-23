6df5e200b130c9ff02e9f3bb447c2559e201158d
sumitpathak914 <sumitpathakofficial914@gmail.com>
2023-03-23 10:09:18 +0530

    add:new changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,19 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React sumit pathak</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR 
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </div>
   )
 }
